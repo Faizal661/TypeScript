2562c0eddac16d70d1ee1df9fcfd00178e7b0c7a
faizal <faisalt661@gmail.com>
2024-11-14 20:01:13 +0530

    typeScript started

---
+++ intro.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = { name: "faizal", age: 20 };
+console.log(user.name);
+var num = 10;
+user.name = user.name.toUpperCase();
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,11 @@
+let user={name:"faizal",age:20};
+
+console.log(user.name);
+
+
+let num=10;
+user.name=user.name.toUpperCase();
+
+console.log(user.name);
+
+export {}
